bc7a75e687f12d8dc7d7e141af89e897471c2c75
yoonhyeyoon <hyeyun3306@gmail.com>
2021-08-09 23:57:42 +0900

    210809 TIL

---
+++ algorithm/SWEA/list_12443.py
@@ -0,0 +1,14 @@
+n = int(input())
+for num in range(n):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    result = 0
+    for i in range(N-1):
+        cnt = N-i-1
+        for j in range(i+1, N-1):
+            if lst[i] <= lst[j]:
+                cnt -= 1
+        if result < cnt:
+            result = cnt
+    print(f'#{num+1} {result}')
+
---
+++ algorithm/SWEA/list_12445.py
@@ -0,0 +1,27 @@
+n = int(input())
+for num in range(n):
+    a = list(map(int, input().strip()))
+    cnt = [0] * 10
+    for val in a:
+        cnt[val] += 1
+    runn = 0
+    triplet = 0
+    i = 0
+    while i < 10:
+        if cnt[i] >= 3:
+            cnt[i] -= 3
+            triplet += 1
+            continue
+        if i < 8:
+            if cnt[i] >= 1 and cnt[i + 1] >= 1 and cnt[i + 2] >= 1:
+                runn += 1
+                cnt[i] -= 1
+                cnt[i + 1] -= 1
+                cnt[i + 2] -= 1
+                continue
+        i += 1
+
+    if runn + triplet == 2:
+        print(f"#{num + 1} Baby Gin")
+    else:
+        print(f"#{num + 1} Lose")
