733446443245979d441800c24951d0c7a86e0707
yoonhyeyoon <hyeyun3306@gmail.com>
2021-12-10 23:57:53 +0900

    211210 TIL

---
+++ java/day1/HelloWorld.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class HelloWorld {
+	
+	// 자바 프로그램 동작의 시작점
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// sysout + ctrl + space
+		System.out.println("Hi Java World");
+		// 자바 - 객체지향 언어 - 객체 : 멤버 변수, 메서드를 가진다.
+		// . --> ~~가 가지고 있는 ...
+	}
+
+}
---
+++ java/day1/Main.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int intType = 100;
+		double doubleType = 150.2;
+		String stringType = "윤혜윤";
+		
+		System.out.println(stringType);
+
+	}
+
+}
