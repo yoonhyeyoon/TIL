a2b842afc4e19a1fc0bc73e9029bd26cf8c37551
yoonhyeyoon <hyeyun3306@gmail.com>
2021-12-20 23:55:55 +0900

    211220 TIL

---
+++ algorithm/programmers/sort/K번쨰수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    
+    for command in commands:
+        i, j, k = command
+        arr = array[i-1:j]
+        arr.sort()
+        answer.append(arr[k-1])
+        
+    return answer
